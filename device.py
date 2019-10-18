19628c91a583431e1d8aa9dc6e9cc95c7e79b9a5
phuctu1901 <phuctu1901@gmail.com>
2019-10-18 09:06:27 +0700

    button event

--- device.py
+++ device.py
@@ -7,23 +7,84 @@ import json
 import dht11
 import RPi.GPIO as GPIO
 
-#config for GPIO
+
+# Khai báo các biến liên quan đến chân GPIO
+
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-instance = dht11.DHT11(pin=23)
 
-RELAIS_1_GPIO = 12
-GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # off
+RELAIS_1_GPIO = 12 #Relay 1 tương ứng với chân GPIO 12
+RELAIS_2_GPIO = 16 #Relay 2 tương ứng với chân GPIO 16
+instance = dht11.DHT11(pin=23) #Cảm biến DHT11 tương ứng với chân GPIO 23
+
+BUTTON_1_GPIO = 16
+BUTTON_2_GPIO = 12
+        
+def button_callback_relay2(channel):
+    print("Button was pushed!")
+    print(channel)
+    if (GPIO.input(BUTTON_1_GPIO)):
+        GPIO.output(RELAIS_2_GPIO, GPIO.LOW) # off
+    else:
+        GPIO.output(RELAIS_2_GPIO, GPIO.HIGH) # on
+    send_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    print(send_time)
+    button_data = {"button": 2, "value": GPIO.input(RELAIS_2_GPIO),"time": send_time}
+    success1 = client.publishEvent("buttonSwitch", "json", button_data, qos=0, onPublish=myOnPublishCallback)
+    if not success1:
+        print("Not connected to IoTF")
+
+def button_callback_relay1(channel):
+    print("Button was pushed!")
+    print(channel)
+    if (GPIO.input(BUTTON_2_GPIO)):
+        GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # off
+    else:
+        GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+    send_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    print(send_time)
+    button_data = {"button": 1, "value": GPIO.input(RELAIS_1_GPIO),"time": send_time}
+    success1 = client.publishEvent("buttonSwitch", "json", button_data, qos=0, onPublish=myOnPublishCallback)
+    if not success1:
+        print("Not connected to IoTF")
+
+def configButton():
+    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    GPIO.add_event_detect(24,GPIO.RISING,callback=button_callback_relay1,  bouncetime=200)
+
+    GPIO.add_event_detect(25,GPIO.RISING,callback=button_callback_relay2,  bouncetime=200)
 
 
-RELAIS_2_GPIO = 16
-GPIO.setup(RELAIS_2_GPIO, GPIO.OUT) # GPIO Assign mode
-GPIO.output(RELAIS_2_GPIO, GPIO.HIGH) # off
+def configRelay():
+    GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # off
 
 
+    GPIO.setup(RELAIS_2_GPIO, GPIO.OUT) # GPIO Assign mode
+    GPIO.output(RELAIS_2_GPIO, GPIO.HIGH) # off
+
+    
+
+def configDevice():
+    configButton()
+    configRelay()
+    
+
+
+#config for GPIO
+
+
+
+
+
+
+
+configDevice()
 
 isPublishEvent = 1
 
@@ -42,15 +103,9 @@ def myCommandCallback(cmd):
 
 
     if(event == "button1"):
-        if (GPIO.input(12)):
-             GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # off
-        else:
-             GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+        button_callback_relay1(0)
     if(event == "button2"):
-        if (GPIO.input(16)):
-             GPIO.output(RELAIS_2_GPIO, GPIO.LOW)
-        else:
-             GPIO.output(RELAIS_2_GPIO, GPIO.HIGH)
+        button_callback_relay2(0)
 
 	
 # Configure
@@ -80,71 +135,27 @@ myConfig = {
 client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
 client.commandCallback = myCommandCallback
 
-
-
-# Connect
 client.connect()
-
-# # Send Data
-# for x in range(2, 30, 3):
-#     myData={'name' : 'foo', 'cpu' : x, 'mem' : 50}
-#     client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=2, onPublish=None)
-
-
-# Connect and send datapoint(s) into the cloud
-# deviceCli.connect()
-# for x in range(0, 1000):
-
-
-def button_callback_relay1(channel):
-    print("Button was pushed!")
-    print(channel)
-    if (GPIO.input(12)):
-        GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # off
-    else:
-        GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
-        
-def button_callback_relay2(channel):
-    print("Button was pushed!")
-    print(channel)
-    if (GPIO.input(16)):
-        GPIO.output(RELAIS_2_GPIO, GPIO.LOW) # off
-    else:
-        GPIO.output(RELAIS_2_GPIO, GPIO.HIGH) # on
-      
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-GPIO.add_event_detect(24,GPIO.RISING,callback=button_callback_relay1,  bouncetime=200)
-
-GPIO.add_event_detect(25,GPIO.RISING,callback=button_callback_relay2,  bouncetime=200)
-# Disconnect
 
 while True:
     while isPublishEvent==1:
         result = instance.read()
+        print(result)
         while result.is_valid():
-            print("Temperature: %-3.1f C" % result.temperature)
-            print("Humidity: %-3.1f %%" % result.humidity)
-            current_time = datetime.datetime.now()
-            env_sensor = {"temp": result.temperature, "hum": result.humidity}
+            send_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+            print(send_time)
+            dht11_data = {"temp": result.temperature, "hum": result.humidity, "time": send_time}
             def myOnPublishCallback():
-            	print("Confirmed event %s received by IoTF\n" % current_time)
-            success1 = client.publishEvent("env_sensor", "json", env_sensor, qos=0, onPublish=myOnPublishCallback)
+                current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+                print("Confirmed event %s received by IoTF\n" % current_time)
+            success1 = client.publishEvent("dht11Data", "json", dht11_data, qos=0, onPublish=myOnPublishCallback)
             if not success1:
                 print("Not connected to IoTF")
-            
             break
-        
-    # data = {"simpledev": "ok", "x": temp}
-
-        
-
-    # success = client.publishEvent("test", "json", data, qos=0, onPublish=myOnPublishCallback)
-    # if not success:
-    #     print("Not connected to IoTF")
-        
-        
         time.sleep(10)
 
 client.disconnect();
+
+def main():
+    print("hello world")
+main()
